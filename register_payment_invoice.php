d8f4c17ee875be736b4f458cf793fc46039eb733
rachmadona48 <rayci232@gmail.com>
2019-11-26 15:52:26 +0700

    register payment invoice

--- register_payment_invoice.php
+++ register_payment_invoice.php
@@ -9,25 +9,26 @@ $common->version();
 $uid = $common->authenticate($db, $username, $password, array());
 $models = ripcord::client("$url/xmlrpc/2/object");
 
-$id_invoice = 233; 
-$partner_id = 1738;
+$payment_method_id = 1; #jangan di ganti
 
+$id_invoice = 23; #id dari invoice yang akan di validate
+$partner_id = 105;
 $payment_date = '2019-11-26';
-$invoice_ids = 233; #id dari invoice yang akan di validate
-$amount = 50000;
+$amount = 1000000;
 $journal_id = 19; #id jurnal
-$communication = 'payment invoice';
-$payment_id = $models.execute_kw($db, $uid, $password, 'account.payment', 'create',
+$communication = 'pembayaran invoice';
+$payment_id = $models->execute_kw($db, $uid, $password, 'account.payment', 'create',
  	array(array(
     'payment_date' => $payment_date,
     'has_invoices' => True,
-    'invoice_ids' =>array(array(6,0,array($invoice_ids))),
+    'invoice_ids' =>array(array(6,0,array($id_invoice))),
     'amount' => $amount,
     'communication' => $communication,
     'journal_id' => $journal_id,
     'partner_type' => 'customer',
     'partner_id' => $partner_id,
     'payment_type' => 'inbound',
+    'payment_method_id' => $payment_method_id,
 
     
     )));
