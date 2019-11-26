462a55377576f1d13caced0a0ec42bf6110ca7d9
rachmadona48 <rayci232@gmail.com>
2019-11-26 15:06:28 +0700

    update 4

--- konek.php
+++ konek.php
@@ -1,15 +1,15 @@
 <?php
  // local dn
-$url = "http://localhost:8071";
-$db = "db12_inagro";
-$username = "user_api";
-$password = "api123";
+// $url = "http://localhost:8071";
+// $db = "db12_inagro";
+// $username = "user_api";
+// $password = "api123";
 
 // server lira
-// $url = "http://10.100.1.65:8069";
-// $db = "db12_bec";
-// $username = "user_api";
-// $password = "api321!#!";
+$url = "http://10.100.1.65:8069";
+$db = "db12_bec";
+$username = "user_api";
+$password = "api321!#!";
 
 
 ?>
