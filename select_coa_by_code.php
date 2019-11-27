2f569b7a18e56c000b0eef8d3594d245580a9ffb
rachmadona48 <rayci232@gmail.com>
2019-11-27 17:01:50 +0700

    show coa by code

---
+++ select_coa_by_code.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once('konek.php');
+
+require_once('ripcord/ripcord.php');
+$common = ripcord::client("$url/xmlrpc/2/common");
+$common->version();
+
+$uid = $common->authenticate($db, $username, $password, array());
+$models = ripcord::client("$url/xmlrpc/2/object");
+
+$code = '200000';
+
+$data = $models->execute_kw($db, $uid, $password,
+    'account.account', 'search_read',
+    array(array(array('deprecated', '=', false),
+    			array('code', '=', $code)
+            )),
+    array('fields'=>array('id','code','name','user_type_id')));
+
+// print_r($data);
+echo json_encode($data)
+
+?>
