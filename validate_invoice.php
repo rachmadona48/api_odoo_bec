1d4b18601f9a68a2a693fd14a996f9dacba1a11c
rachmadona48 <rayci232@gmail.com>
2019-11-29 19:17:25 +0700

    perbaiki validate invoice

--- validate_invoice.php
+++ validate_invoice.php
@@ -9,25 +9,19 @@ $common->version();
 $uid = $common->authenticate($db, $username, $password, array());
 $models = ripcord::client("$url/xmlrpc/2/object");
 
-$id_invoice = 202; #id dari invoice yang akan di validate
+$id_invoice = 24; #id dari invoice yang akan di validate
 
 $data = $models->execute_kw($db, $uid, $password,'account.invoice', 'action_invoice_open', [$id_invoice]);
 
-$data_return = [];
-if ($data == 1){
-	$return  = 'Sukses';
-	$data_return = $models->execute_kw($db, $uid, $password,
+
+$data_return = $models->execute_kw($db, $uid, $password,
     'account.invoice', 'search_read',
     array(array(array('id', '=', $id_invoice)
             )),
-    array('fields'=>array('id','number')));
-}else{
-	$return  = 'Gagal';
-}
-
+    array('fields'=>array('id','state')));
 #jika return = Sukses maka baca number dari data_return
 
-echo $return;
-echo json_encode($data_return);
+// echo $data;
+echo json_encode($data_return); #jika state = open maka proses validate invoice berhasil
 
 ?>
