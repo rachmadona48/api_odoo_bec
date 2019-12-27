3f5a037570a1fae09b3d28e2a6ec87c85e032953
rachmadona48 <rayci232@gmail.com>
2019-12-27 21:58:59 +0700

    approval cancel invoice 2

--- compute_unreconciled_invoice_ids.php
+++ compute_unreconciled_invoice_ids.php
@@ -9,16 +9,25 @@ $common->version();
 $uid = $common->authenticate($db, $username, $password, array());
 $models = ripcord::client("$url/xmlrpc/2/object");
 
-$id_invoice = 135; #id invoice yang akan dicancel
+$id_invoice = 131; #id invoice yang akan dicancel
 
 $data = $models->execute_kw($db, $uid, $password,'account.invoice', 'action_cancel_via_service', [$id_invoice]);
 // echo json_encode($data); 
 
-$data_payment_invoice = $models->execute_kw($db, $uid, $password,
+$data_payment_invoice_active = $models->execute_kw($db, $uid, $password,
     'account.invoice', 'search_read',
-    array(array(array('id', '=', $id_invoice)
+    array(array('&',array('id', '=', $id_invoice),
+    			array('active', '=',False),
             )),
     array('fields'=>array('id','state','active')));
 
-echo json_encode($data_payment_invoice);
+$data_payment_invoice_non_active = $models->execute_kw($db, $uid, $password,
+    'account.invoice', 'search_read',
+    array(array('&',array('id', '=', $id_invoice),
+    			array('active', '=',True),
+            )),
+    array('fields'=>array('id','state','active')));
+$return  = array('data_payment_invoice_active' => $data_payment_invoice_active,'data_payment_invoice_non_active' => $data_payment_invoice_non_active );
+
+echo json_encode($return);
 ?>
