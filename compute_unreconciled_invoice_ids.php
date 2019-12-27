080a4d12296cf3b0d7cf1bc8b57ac5b0c95130d4
rachmadona48 <rayci232@gmail.com>
2019-12-27 22:05:26 +0700

    persetujuan untuk cancel invoice 2

--- compute_unreconciled_invoice_ids.php
+++ compute_unreconciled_invoice_ids.php
@@ -9,19 +9,19 @@ $common->version();
 $uid = $common->authenticate($db, $username, $password, array());
 $models = ripcord::client("$url/xmlrpc/2/object");
 
-$id_invoice = 131; #id invoice yang akan dicancel
+$id_invoice = 123; #id invoice yang akan dicancel
 
 $data = $models->execute_kw($db, $uid, $password,'account.invoice', 'action_cancel_via_service', [$id_invoice]);
 // echo json_encode($data); 
 
-$data_payment_invoice_active = $models->execute_kw($db, $uid, $password,
+$data_payment_invoice_non_active = $models->execute_kw($db, $uid, $password,
     'account.invoice', 'search_read',
     array(array('&',array('id', '=', $id_invoice),
     			array('active', '=',False),
             )),
     array('fields'=>array('id','state','active')));
 
-$data_payment_invoice_non_active = $models->execute_kw($db, $uid, $password,
+$data_payment_invoice_active = $models->execute_kw($db, $uid, $password,
     'account.invoice', 'search_read',
     array(array('&',array('id', '=', $id_invoice),
     			array('active', '=',True),
